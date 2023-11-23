3ba206419ab85bbe591d6d00a5a31a4f91d7e773
mirae <mirae9873@gmail.com>
2023-11-23 13:10:11 +0900

    todo 추가 가능

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -2,7 +2,7 @@ import { createStore } from "redux";
 import { combineReducers } from "redux";
 import todos from "../modules/todos";
 
-const rootReducer = combineReducers(todos);
+const rootReducer = combineReducers({ todos });
 
 const store = createStore(rootReducer);
 
